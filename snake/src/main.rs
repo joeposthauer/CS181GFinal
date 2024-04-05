c35ef2b3ea62053a685d88b441997c0a89b87622
joeposthauer <posthauerjoe@gmail.com>
2024-04-05 11:54:23 -0700

    adding more basic setup stuff

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,3 +1,5 @@
+use assets_manager::{asset::Png, AssetCache};
+use std::str::FromStr;
 use frenderer::{
     input::{Input, Key},
     sprites::{Camera2D, SheetRegion, Transform},
@@ -15,10 +17,103 @@ enum EntityType {
     Food,
 }
 
+struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
 struct Game {
     started: bool,
 }
 
+struct Level {
+    name: String,
+    bg: SheetRegion,
+    grid: Grid<u8>,
+    tileset: Tileset,
+    starts: Vec<(EntityType, Vec2)>,
+}
+
+pub struct Grid<T> {
+    width: usize,
+    height: usize,
+    storage: Box<[T]>,
+}
+
+struct Tileset {
+    tiles: Vec<TileData>,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub w: u16,
+    pub h: u16,
+}
+
 fn main() {
     println!("Ayelet - Change test");
+}
+
+impl Game {
+    fn new(renderer: &mut Renderer, cache: &AssetCache) -> Self {
+        let tile_handle = cache
+            .load::<Png>("texture")
+            .expect("Couldn't load tilesheet img");
+        let tile_img = tile_handle.read().0.to_rgba8();
+        let tile_tex = renderer.create_array_texture(
+            &[&tile_img],
+            wgpu::TextureFormat::Rgba8UnormSrgb,
+            tile_img.dimensions(),
+            Some("tiles-sprites"),
+        );
+        let levels = vec![
+            Level::from_str(
+                &cache
+                    .load::<String>("level")
+                    .expect("Couldn't access level.txt")
+                    .read(),
+            ),
+        ];
+        let current_level = 0;
+        let camera = Camera2D {
+            screen_pos: [0.0, 0.0],
+            screen_size: [W as f32, H as f32],
+        };
+        let sprite_estimate =
+            levels[current_level].sprite_count() + levels[current_level].starts().len();
+        renderer.sprite_group_add(
+            &tile_tex,
+            vec![Transform::ZERO; sprite_estimate],
+            vec![SheetRegion::ZERO; sprite_estimate],
+            camera,
+        );
+        let player_start = *levels[current_level]
+            .starts()
+            .iter()
+            .find(|(t, _)| *t == EntityType::Player)
+            .map(|(_, ploc)| ploc)
+            .expect("Start level doesn't put the player anywhere");
+        let mut game = Game {
+            current_level,
+            attack_area: Rect {
+                x: 0.0,
+                y: 0.0,
+                w: 0,
+                h: 0,
+            },
+            knockback_timer: 0.0,
+            attack_timer: 0.0,
+            levels,
+            health: 3,
+            enemies: vec![],
+            player: Pos {
+                pos: player_start,
+                dir: Dir::S,
+            },
+        };
+        game.enter_level(player_start);
+        game
+    }
 }
