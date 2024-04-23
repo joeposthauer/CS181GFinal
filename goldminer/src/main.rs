3a68a21612b08f2b377de0098f95df12353e8ddc
joeposthauer <posthauerjoe@gmail.com>
2024-04-23 10:21:50 -0700

    added stuff

--- goldminer/src/main.rs
+++ goldminer/src/main.rs
@@ -1,3 +1,18 @@
+// use assets_manager::{asset::Png, AssetCache};
+use frenderer::{
+    input::{Input, Key},
+    sprites::{Camera2D, SheetRegion, Transform},
+    wgpu, Immediate, Renderer,
+};
+
+use engine::{grid::Grid, *};
+use engine::{level::Level, *};
+
+const TILE_SZ: usize = 4;
+const W: usize = 120;
+const H: usize = 120;
+const DT: f32 = 1.0 / 60.0;
+
 fn main() {
     println!("Hello, world!");
 }
