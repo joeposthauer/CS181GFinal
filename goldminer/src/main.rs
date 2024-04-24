23c79239d1d9cfdf985b7109974ecd8bdcb2f02b
joeposthauer <posthauerjoe@gmail.com>
2024-04-23 23:44:52 -0700

    added to main

--- goldminer/src/main.rs
+++ goldminer/src/main.rs
@@ -1,4 +1,4 @@
-// use assets_manager::{asset::Png, AssetCache};
+use assets_manager::{asset::Png, AssetCache};
 use frenderer::{
     input::{Input, Key},
     sprites::{Camera2D, SheetRegion, Transform},
@@ -20,6 +20,15 @@ pub enum EntityType {
     Gold,
     Silver,
     Rock,
+}
+
+struct Game {
+    claw: Claw,
+    score: usize,
+    current_level: usize,
+    levels: Vec<Level>,
+    entities: Vec<EntityType>,
+    timer: usize,
 }
 struct Claw {
     dir: Dir,
@@ -66,5 +75,73 @@ const SILVER: [SheetRegion; 1] = [SheetRegion::rect(190, 345, 4, 4)];
 const ROCK: [SheetRegion; 1] = [SheetRegion::rect(190, 345, 4, 4)];
 
 fn main() {
-    println!("Hello, world!");
+    #[cfg(not(target_arch = "wasm32"))]
+    let source =
+        assets_manager::source::FileSystem::new("content").expect("Couldn't load resources");
+    #[cfg(target_arch = "wasm32")]
+    let source = assets_manager::source::Embedded::from(assets_manager::source::embed!("content"));
+    let cache = assets_manager::AssetCache::with_source(source);
+
+    let drv = frenderer::Driver::new(
+        winit::window::WindowBuilder::new()
+            .with_title("test")
+            .with_inner_size(winit::dpi::LogicalSize::new(1024.0, 768.0)),
+        Some((W as u32, H as u32)),
+    );
+
+    let mut input = Input::default();
+
+    let mut now = frenderer::clock::Instant::now();
+    let mut acc = 0.0;
+    drv.run_event_loop::<(), _>(
+        move |window, frend| {
+            let mut frend = Immediate::new(frend);
+            let game = Game::new(&mut frend, cache);
+            (window, game, frend)
+        },
+        move |event, target, (window, ref mut game, ref mut frend)| {
+            use winit::event::{Event, WindowEvent};
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    target.exit();
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    if !frend.gpu().is_web() {
+                        frend.resize_surface(size.width, size.height);
+                    }
+                    window.request_redraw();
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    let elapsed = now.elapsed().as_secs_f32();
+                    // You can add the time snapping/death spiral prevention stuff here if you want.
+                    // I'm not using it here to keep the starter code small.
+                    acc += elapsed;
+                    now = std::time::Instant::now();
+                    // While we have time to spend
+                    while acc >= DT {
+                        // simulate a frame
+                        acc -= DT;
+                        game.simulate(&input, DT);
+                        input.next_frame();
+                    }
+                    game.render(frend);
+                    frend.render();
+                    window.request_redraw();
+                }
+                event => {
+                    input.process_input_event(&event);
+                }
+            }
+        },
+    )
+    .expect("event loop error");
 }
